3df211ef7a5135c93d30a3a8e74c7c3ed73b7caf
LimLan <sm.m.panoramik@gmail.com>
2021-01-23 18:22:57 +0300

    init commit

---
+++ UnityDummyProject/Assets/Scripts/WindowScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class WindowScript : MonoBehaviour
+{
+      public TextMeshProUGUI text;
+      public TextMeshProUGUI disabledText;
+
+      public void Start()
+      {
+            text.text = disabledText.text;
+      }
+}
